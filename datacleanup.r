7deb61422c7b4d9d1e79eea8cc680aeda5124d9f
higginsc20 <higginsc20@mail.wlu.edu>
2019-10-24 13:10:51 -0400

    More Cleaning.

--- datacleanup.r
+++ datacleanup.r
@@ -9,7 +9,7 @@ movies <- read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/maste
 # data clean-up
 moviesclean <- movies
 moviesclean$title <- gsub("&amp;", "&", moviesclean$title)
-moviesclean$title <- gsub( "&#39", "'", moviesclean$title)
+moviesclean$title <- gsub( "&#39;", "'", moviesclean$title)
 
 #pushing to you
 
