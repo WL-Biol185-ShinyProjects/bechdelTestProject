d1c74bd2cb6936d69fc67457be320b0855010788
higginsc20 <higginsc20@mail.wlu.edu>
2019-10-24 13:01:33 -0400

    Addition of Packages.

--- datacleanup.r
+++ datacleanup.r
@@ -1,8 +1,17 @@
+#packages
+library(tidyverse)
+library(shiny)
+library(ggplot2)
+
+
 # download data
 movies <- read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/bechdel/movies.csv")
 
 # data clean-up
+moviesclean <- movies
 moviesclean$title <- gsub("&amp;", "&", moviesclean$title)
 moviesclean$title <- gsub( "&#39", "'", moviesclean$title)
 
 #pushing to you
+
+#deleting IMDB Columns
