104b1a291dee71fbf648b461d0b00d09c4ef8c27
ET <thompsone20@wlu.edu>
2019-10-24 13:07:19 -0400

    renamed movies

--- datacleanup.r
+++ datacleanup.r
@@ -2,6 +2,7 @@
 movies <- read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/bechdel/movies.csv")
 
 # data clean-up
+moviesclean <- movies
 moviesclean$title <- gsub("&amp;", "&", moviesclean$title)
 moviesclean$title <- gsub( "&#39", "'", moviesclean$title)
 
