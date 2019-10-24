d9122e3af56f7adf1cec4b0cabd26d3f0e290a21
Rebecca Telese <teleser20@mail.wlu.edu>
2019-10-24 13:05:36 -0400

    got rid of IMDB

--- datacleanup.r
+++ datacleanup.r
@@ -2,7 +2,11 @@
 movies <- read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/bechdel/movies.csv")
 
 # data clean-up
+moviesclean <- movies
 moviesclean$title <- gsub("&amp;", "&", moviesclean$title)
 moviesclean$title <- gsub( "&#39", "'", moviesclean$title)
 
 #pushing to you
+
+#removing IMDB
+moviesclean$imdb <- NULL
