d5acf0e9fdc9580492e59cbe2a710f784cfaa843
ET <thompsone20@wlu.edu>
2019-10-24 12:57:56 -0400

    commit

---
+++ datacleanup.r
@@ -0,0 +1,8 @@
+# download data
+movies <- read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/bechdel/movies.csv")
+
+# data clean-up
+moviesclean$title <- gsub("&amp;", "&", moviesclean$title)
+moviesclean$title <- gsub( "&#39", "'", moviesclean$title)
+
+#pushing to you
