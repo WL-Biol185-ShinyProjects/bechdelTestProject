60a00a037fd8f8e2d9f60d1759e51810d052f1ec
ET <thompsone20@wlu.edu>
2019-11-05 13:07:55 -0500

    something

--- server.r
+++ server.r
@@ -10,22 +10,11 @@ library(ggplot2)
   # Application title
   titlePanel("Bechdel Test Data")
   
-<<<<<<< HEAD
-    
-    # generate bins based on input$bins from ui.R
-    movieData    <- moviescleanest
-    range <- moviescleanest$budget(
-      min(7000),
-      max(425000000), 
-    )
-=======
->>>>>>> 0defce256a5c8b2be3f7afa0aaee29d7ca77ff07
-    
 
     # draw the histogram   
    
-  moviescleanest %>%
-    ggplot(aes(budget)) + geom_histogram()
+    moviescleanest %>%
+      ggplot(aes(budget)) + geom_histogram()
   
   
 
