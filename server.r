a1772381e026853389765dfce582afb220f68fb2
Rebecca Telese <teleser20@mail.wlu.edu>
2019-11-05 13:29:51 -0500

    change

--- server.r
+++ server.r
@@ -13,15 +13,9 @@ library(ggplot2)
 
     # draw the histogram   
    
-<<<<<<< HEAD
-    moviescleanest %>%
-      ggplot(aes(budget)) + geom_histogram()
-  
-=======
   moviescleanest %>%
     ggplot(aes(year, budget)) + geom_point()
->>>>>>> 69e40a73e345c08c1198b11b1fd183f0edc2b02e
-  
+
   hover <- input$"moviescleanest$title"
 
   }
