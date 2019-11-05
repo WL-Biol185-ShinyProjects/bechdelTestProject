e02b7315e13856eaf39a87764f2e8c84d5c33b37
Rebecca Telese <teleser20@mail.wlu.edu>
2019-11-05 14:06:58 -0500

    group

--- server.r
+++ server.r
@@ -10,9 +10,13 @@ library(ggplot2)
     # Application title
     titlePanel("Bechdel Test Data")
     
+    budgetLevels <- list(Low Budget= 7000:212000000, High Budget = 212000001:425000000)
 
     # draw the histogram   
     moviescleanest %>%
+      filter(
+        "Low Budget", "High Budget" 
+        ) %>%
       ggplot(aes(year, budget)) + geom_point()
 
 
