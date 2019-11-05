dfe7c1e5690ddc3b217e5c11a3ac26764e90cb40
Rebecca Telese <teleser20@mail.wlu.edu>
2019-11-05 12:08:12 -0500

    fixed bracket errors

--- server.r
+++ server.r
@@ -7,7 +7,7 @@ function(input, output) {
   output$distPlot <- renderPlot({
 
   # Application title
-  titlePanel("Bechdel Test Data"),
+  titlePanel("Bechdel Test Data")
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
@@ -21,17 +21,17 @@ function(input, output) {
 
     
     # generate bins based on input$bins from ui.R
-    x    <- moviescleanest
+    x    <- moviescleanest,
     range <- moviescleanest$budget(
       min(x),
       max(x), 
-    )
+    ),
     
     sliderInput("range",
                 "Range:",
                 min   = 1000, 
                 max   = 10000000,
-                value = c(1500,100000))
+                value = c(1500,100000)),
      
     # draw the histogram with the specified number of bins
     hist(x, breaks = range, col = 'darkgray', border = 'white')
@@ -40,4 +40,7 @@ function(input, output) {
   
 
   )}
+  
+  )
+  
 }
