51edf288922db6b20330e2fe80d296021d778714
higginsc20 <higginsc20@mail.wlu.edu>
2019-11-05 12:00:48 -0500

    Server, UI changes

--- server.r
+++ server.r
@@ -1,26 +1,26 @@
 library(shiny)
-library(tidyverse)
-library(ggplot2)
 
-# Define UI for application that draws a histogram
-fluidPage(
+# Define server logic required to draw a histogram
+function(input, output) {
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("range",
-                  "Range:",
-                  min   = 1, 
-                  max   = 1000,
-                  value = c(200,500)),
-    ),
+  output$distPlot <- renderPlot({
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+    # generate bins based on input$bins from ui.R
+    x    <- moviescleanest
+    range <- moviescleanest$budget(
+      min(x),
+      max(x), 
     )
-  )
-)
+    
+    sliderInput("range",
+                "Range:",
+                min   = 1000, 
+                max   = 10000000,
+                value = c(1500,100000))
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = range, col = 'darkgray', border = 'white')
+    
+  })
+  
+}
--- ui.r
+++ ui.r
@@ -1,17 +1,22 @@
 library(shiny)
+library(tidyverse)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-function(input, output) {
+# Define UI for application that draws a histogram
+fluidPage(
   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
+  # Application title
+  titlePanel("Bechdel Test Data"),
   
-}
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
