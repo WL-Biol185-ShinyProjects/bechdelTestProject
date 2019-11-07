71a1e2a7dac97028ce732554f5a6e6a86c24d40a
ET <thompsone20@wlu.edu>
2019-11-07 12:35:29 -0500

    trying to change background color

--- server.r
+++ server.r
@@ -2,29 +2,41 @@ library(shiny)
 library(ggplot2)
 
 
+
   # Define server logic required to draw a histogram
+
   function(input, output) {
+
   
 
   output$distPlot <- renderPlot({
+    
+
 
     # Application title
     titlePanel("Bechdel Test Data")
     
 
 
+
+
     # draw the histogram  
+    
+
     moviescleanest %>%
       ggplot(aes(year, budget)) + geom_point()
     
   
 
     # draw the histogram   
-      ggplot(moviescleanest, aes(year, input$budget)) + geom_point()
+      ggplot(moviescleanest, aes(year,budget)) + geom_point()
+
 
 
 
   })
+  
+
   
 
   
