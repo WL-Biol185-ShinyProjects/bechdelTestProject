6ef22b2053b3e0b01a3b60ea3da5cdefad1922da
Rebecca Telese <teleser20@mail.wlu.edu>
2019-11-12 13:18:29 -0500

    trying to connect slider

--- server.r
+++ server.r
@@ -1,28 +1,65 @@
 library(shiny)
 library(ggplot2)
+library(dplyr)
 
 
   # Define server logic required to draw a histogram
-  function(input, output) {
+  function(input, output, session) {
   
 
-  output$distPlot <- renderPlot({
+  output$lowBudgetPlot <- renderPlot({
 
     # Application title
       titlePanel("Bechdel Test Data")
     
 
-    # draw the graph   
-     ggplot(moviescleanest, aes(year, input$budget)) + geom_point()
-      
-      
+    # draw the graphs
+    moviescleanest %>%
+      filter() %>%
+     ggplot(aes(year, budget)) + geom_point()
     
+
   
     })
 
-
+  output$highBudgetPlot <- renderPlot({
+    
+    # Application title
+    titlePanel("Bechdel Test Data")
+    
+    
+    # draw the graphs
+    ggplot(moviescleanest, aes(year, moviescleanest$budget)) + geom_point()
+    
+    
+    
+  })
   
-
+  output$domGrossPlot <- renderPlot({
+    
+    # Application title
+    titlePanel("Bechdel Test Data")
+    
+    
+    # draw the graphs
+    ggplot(moviescleanest, aes(year,  moviescleanest$domgross)) + geom_point()
+    
+    
+    
+  })
+  
+  output$intGrossPlot <- renderPlot({
+    
+    # Application title
+    titlePanel("Bechdel Test Data")
+    
+    
+    # draw the graphs
+    ggplot(moviescleanest, aes(year,  moviescleanest$intgross)) + geom_point() + xlim(1970, 2013)
+    
+    
+    
+  })
  
   }
   
