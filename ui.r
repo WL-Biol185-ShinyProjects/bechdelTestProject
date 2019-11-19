6eff93c307ac49939af8b5ece90965123930acd2
Rebecca Telese <teleser20@mail.wlu.edu>
2019-11-19 11:41:15 -0500

    fixed size of comic

--- ui.r
+++ ui.r
@@ -22,7 +22,7 @@ library(ggplot2)
         tabItems(
         tabItem(tabName = "homeTab", 
                 "The Bechdel Test was created in response to this cartoon by Alison Bechdel in 1985. This test is a measure of the representation of women in movies based on three facets. The movie must contain at least two women, these two women must have a conversation with one another, and this conversation must be about something other than a man.", 
-        img(src = "https://upload.wikimedia.org/wikipedia/en/b/bf/Dykes_to_Watch_Out_For_%28Bechdel_test_origin%29.jpg", height = 300, width = 500, align = "center")
+        img(src = "https://upload.wikimedia.org/wikipedia/en/b/bf/Dykes_to_Watch_Out_For_%28Bechdel_test_origin%29.jpg", height = 600, width = 1000, align = "center")
         ),
         tabItem(tabName = "sourcesTab", "sources will go here"),
         tabItem(tabName = "dataTab",
