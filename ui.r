fccb7df02443b5e9ff77bed127b0705225345a79
higginsc20 <higginsc20@mail.wlu.edu>
2019-11-21 15:45:35 -0500

    Adding descriptions and spacing.

--- ui.r
+++ ui.r
@@ -4,28 +4,29 @@ library(tidyverse)
 library(ggplot2)
 library(htmltools)
 
+# Pulling Up Data Frame
+
 moviescleanest <- read.csv("moviescleanest.csv")
 
 # Define UI for application that draws a histogram
     shinyUI(fluidPage( 
   
- # Application title
-    
+# Application title
     ui <- dashboardPage(
       dashboardHeader(title = "Bechdel Test"),
       
-      # Making tabs on sidebar
-      
+# Making tabs on sidebar
       dashboardSidebar(
         sidebarMenu(
-         menuItem("Home", tabName = "homeTab"),
-         menuItem("Data", tabName = "dataTab"),
+         menuItem("Home",    tabName = "homeTab"),
+         menuItem("Data",    tabName = "dataTab"),
          menuItem("Sources", tabName = "sourcesTab")
         )
       ),
+
       dashboardBody(
         tabItems(
-        tabItem((tabName = "homeTab"), 
+        tabItem((            tabName = "homeTab"), 
        
               h3(strong("What is the Bechdel Test?")),
        
@@ -48,21 +49,27 @@ moviescleanest <- read.csv("moviescleanest.csv")
                 br(),
                 
                 h4(p("3) this conversation must be about something other than a man")), 
+         
          br(),
+         
          br(),
          
          img(src = "https://upload.wikimedia.org/wikipedia/en/b/bf/Dykes_to_Watch_Out_For_%28Bechdel_test_origin%29.jpg", height = 600, width = 1000, align = "center"),
         
         br(),
+        
         br(),
+        
         br(),
         
         h4(p("All of the Data Points:")),
         
         br(),
+        
         br(),
         
         textOutput('hoverPointInfoAllPlot'),
+        
         plotOutput("allPlot", hover = hoverOpts(id = "allPlotHover")),
         
         
@@ -75,38 +82,42 @@ moviescleanest <- read.csv("moviescleanest.csv")
         
         
         tabItem(tabName = "sourcesTab", 
+                
                 h3(strong("Sources")),
                 
                 br(),
                 br(),
                 
-                a(h4("About Bechdel Test and Original Comic Strip"), href = "wwww.npr.org/templates/story/story.php?storyId=94202522"),
+                a(h4("About Bechdel Test and Original Comic Strip"), 
+                  href  = "wwww.npr.org/templates/story/story.php?storyId=94202522"),
                 
                 br(),
                 br(),
                 
-                a(h4("Article Where Data is From"), href = "https://fivethirtyeight.com/features/the-dollar-and-cents-case-against-hollywoods-exclusion-of-women/"),
+                a(h4("Article Where Data is From"), 
+                  href  = "https://fivethirtyeight.com/features/the-dollar-and-cents-case-against-hollywoods-exclusion-of-women/"),
                 
                 br(),
                 br(),
                 
-                a(h4("Github for Data"), href = "https://github.com/fivethirtyeight/data/tree/master/bechdel")
-                
+                a(h4("Github for Data"),
+                  href  = "https://github.com/fivethirtyeight/data/tree/master/bechdel")
                 
                 ),
                 
         tabItem(tabName = "dataTab",
                 
-                
-                
              sidebarLayout(
+               
                sidebarPanel(
+                 
                  radioButtons("dist", "Reason for Failure:", 
                               c("No Named Women Characters" = "nowomen",
                                 "No Women Talking"          = "notalk",
                                 "Talk Only About Men"       = "men",
                                 "Passed Bechdel Test"       = "ok"
-                                 )),
+                              )),
+                 
                  br(),
                  
                  radioButtons("dec", "Decade:", 
@@ -115,59 +126,52 @@ moviescleanest <- read.csv("moviescleanest.csv")
                                 "1990's" = "1990s",
                                 "2000's" = "2000s",
                                 "2010's" = "2010s"))
-               ),
+                              ),
               
-               
                # Show a plot of the generated distribution
                mainPanel(
                  
                  tabsetPanel(type = "tabs", 
                              
                              tabPanel("Low Budget",
-                                      
                                       plotOutput("lowBudgetPlot", hover = hoverOpts(id = "lowBudgetHover")),
                                       textOutput('hoverPointInfoLow'),
-                                      
                                       sliderInput("lowBudget",
                                                   "Low Budget:",
                                                   7000,
                                                   100000000,
                                                   value = c(11000000, 900000000),
                                                   step = 2500)),
+                             
                              tabPanel("High Budget",
-
                                       plotOutput("highBudgetPlot", hover = hoverOpts(id = "highBudgetHover")),
                                       textOutput('hoverPointInfoHigh'),
-                                      
                                       sliderInput("highBudget",
                                                   "High Budget:",
                                                   100000001,
                                                   425000000,
                                                   value = c(115000000, 242000000),
                                                   step = 2500)),
+                             
                              tabPanel("Domestic Gross",
-                                      
                                       plotOutput("domgrossPlot", hover = hoverOpts(id = "domgrossHover")),
                                       textOutput('hoverPointInfoDomgross'),
-                                      
                                       sliderInput("domgross",
                                                   "Domestic Gross:",
                                                   800,
                                                   500000000,
                                                   value = c(100000000, 200000000),
                                                   step = 2500)),
+                             
                              tabPanel("International Gross", 
-                                      
                                       plotOutput("intGrossPlot", hover = hoverOpts(id = "intGrossHover")),
                                       textOutput('hoverPointInfoIntGross'),
-                                      
                                       sliderInput("intGross",
                                                   "International Gross:",
                                                   800,
                                                   500000000,
                                                   value = c(100000000, 200000000),
                                                   step = 2500))
-                             
                              
                 )
                )
